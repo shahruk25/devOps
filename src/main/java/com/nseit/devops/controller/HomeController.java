e7b66b1c1fbc5404564d560714a4804fae2845a2
shahruk25 <shahruk25@gmail.com>
2020-04-16 18:56:23 +0530

    HomeController Indented

--- src/main/java/com/nseit/devops/controller/HomeController.java
+++ src/main/java/com/nseit/devops/controller/HomeController.java
@@ -6,20 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	
-    @GetMapping("/")
-    public String index() {
-        return "login";
-    }
+	@GetMapping("/")
+	public String index() {
+		return "login";
+	}
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 
-    @GetMapping("/access-denied")
-    public String accessDenied() {
-        return "/error/access-denied";
-    }
+	@GetMapping("/access-denied")
+	public String accessDenied() {
+		return "/error/access-denied";
+	}
 
 }
